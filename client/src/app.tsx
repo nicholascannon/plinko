6d2b5204c07bab6c167259a1d331094b5f22b0b1
nicholascannon <nicholascannon1@gmail.com>
2025-11-12 08:26:47 +1100

    Setup boilerplate pixi react app

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,21 @@
+import { Application, extend } from '@pixi/react';
+import { Graphics } from 'pixi.js';
+
+import './app.css';
+
+extend({ Graphics });
+
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Application antialias resizeTo={window}>
+      <pixiGraphics
+        draw={(graphics) => {
+          graphics.clear();
+          graphics.setFillStyle({ color: 'red' });
+          graphics.rect(0, 0, 100, 100);
+          graphics.fill();
+        }}
+      />
+    </Application>
   );
 }
